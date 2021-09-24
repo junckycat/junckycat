cce53aa06f3471bafd4eb33b4a244eff0c146ba0
Jules <jules.tambon@ynov.comm>
2021-09-24 09:05:03 +0200

    personnage&takepot

---
+++ menuMarchand.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"fmt"
+)
+
+func bienvenue(bvn string, ch1 int, ch2 int) {
+	fmt.Println("Bienvenue sur forhonor Golang!")
+}
+
+type Perso struct {
+	nom    string
+	classe string
+	niveau int
+	pv     int
+	pva    int
+	inv    []string
+}
+
+func (c *Perso) Init(nom string, class string, inv []string, niveau int, pv int, pva int) {
+	c.nom = nom
+	c.classe = class
+	c.niveau = niveau
+	c.pv = pv
+	c.pva = pva
+	c.inv = inv
+}
+
+func main() {
+	var p1 Perso
+	p1.Init("oroshi", "Samurai", []string{"", ""}, 1, 1000, 150)
+	fmt.Println("----------------")
+	p1.DisplayInfo()
+	fmt.Println("----------------")
+	var p2 Perso
+	p2.Init("raider", "viking", []string{"hache,", "potion de vie"}, 1, 1500, 300)
+	p2.DisplayInfo()
+	fmt.Println("----------------")
+}
+
+func (p Perso) DisplayInfo() {
+	fmt.Println("Nom :", p.nom)
+	fmt.Println("classe :", p.classe)
+	fmt.Println("niveau :", p.niveau)
+	fmt.Println("point de vie actuel :", p.pva)
+	fmt.Println("point de vie :", p.pv)
+	fmt.Println("inventaire :", p.inv)
+}
+
+func (p Perso) DisplayInventory() {
+	if len(p.inv) == 0 {
+		fmt.Println("inventaire vide")
+	}
+	for i := 0; i < len(p.inv); i++ {
+		fmt.Println("->", p.inv[i], "x1")
+	}
+}
+func (p *Perso) takePot() {
+	for _, lettre := range p.inv {
+		if lettre == "potion de vie" && p.pv <= p.pv-50 {
+			p.pv += 50
+			p.inv[len(p.inv)-1] = ""
+		}
+	}
+}
+
+func Bienvenue() {
+	var p1 Perso
+	fmt.Println("Bienvenue sur for honor golang!")
+	p1.Init("oroshi", "Samurai", []string{"sabre", "bouclier"}, 1, 1000, 150)
+	fmt.Println("----------------")
+	fmt.Println("Personnage 1 :\nNom :", p1.nom, "\nClasse :", p1.classe)
+	fmt.Println("----------------")
+	var p2 Perso
+	p2.Init("raider", "viking", []string{"hache", "potion"}, 1, 1500, 300)
+	fmt.Println("Personnage 2 :\nNom :", p2.nom, "\nClasse", p2.classe)
+	fmt.Println("----------------")
+	choice := 1
+	var pc Perso
+	if choice == 1 {
+		pc.Init(p1.nom, p1.classe, p1.inv, p1.niveau, p1.pv, p1.pva)
+	} else if choice == 2 {
+		pc.Init(p2.nom, p2.classe, p2.inv, p2.niveau, p2.pv, p2.pva)
+	}
+	fmt.Println("Vous avez choisi le personnage :")
+	fmt.Println("Nom :", pc.nom, "\nClasse :", pc.classe)
+	fmt.Println("---------------")
+	fmt.Println("Menu")
+	fmt.Println("---------------")
+	fmt.Println("1 : Afficher les caratÃ©ristiques")
+	fmt.Println("2 : Afficher l'inventaire du personnage")
+	fmt.Println("3 : Afficher le marcher du IA")
+	fmt.Println("4 : Combatre le IA")
+	fmt.Println("5 : Quitter le jeux")
+	menuChoice := 1
+	switch menuChoice {
+	case 1:
+		pc.DisplayInfo()
+	case 2:
+		fmt.Println("Afficher l'inventaire du personnage")
+	case 3:
+		fmt.Println("Afficher le marcher du IA")
+	case 4:
+		fmt.Println("Combatre le IA")
+	default:
+		fmt.Println("Veuillez entrez un 1 ou 2")
+	case 5:
+		fmt.Println("Quitter le jeux")
+	}
+}
+
+func Menu() {
+}
+
+func (p *Perso) dead() {
+	if p.pva <= 0 {
+		p.pva = p.pv / 2
+		fmt.Println("Vous etes mort, vous réaparaissait avec la moitié de vos points de vies")
+		Menu()
+	}
+}
